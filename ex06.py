b88aca123b093f56caa13381eac692b46eebe5a7
Diêgo Souza <142118500+diegops17@users.noreply.github.com>
2026-02-07 00:40:39 +0000

    resolução ex06 e ex07

---
+++ ex06.py
@@ -0,0 +1,9 @@
+import math
+num = int(input('Informe um número inteiro qualquer: '))
+dobro = num * 2
+triplo = num * 3
+raiz_quadrada = math.sqrt(num)
+
+print(f'O dobro de {num} é: {dobro}')
+print(f'O triplo de {num} é: {triplo}')
+print(f'A raiz quadrada de {num} é: {raiz_quadrada}')
