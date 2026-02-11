a68dd6c163043f1220951a2c02d77fdea31232f9
Diêgo Souza <142118500+diegops17@users.noreply.github.com>
2026-02-11 01:00:28 +0000

    resolução do ex13 e ex14

---
+++ ex13_salario_reajuste.py
@@ -0,0 +1,5 @@
+salario = float(input('Informe o seu salário R$ '))
+percentual_reajuste = 0.15
+novo_salario =  salario + (salario * percentual_reajuste)
+
+print(f'Com reajuste de 15% o novo salário é de R$ {novo_salario} reais')
---
+++ ex14_celsius_fahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input('Informe a temperatura em graus ºC: '))
+
+fahrenheit = ((celsius * 1.8) + 32)
+
+print(f'{celsius}ºC equivale a {fahrenheit}ºF')
